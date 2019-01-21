1ff973ccb176e7d5e10a649907b1132c5969669c
ZaxisFr <nilskrattinger@yahoo.fr>
2019-01-21 13:40:55 +0100

    Debut 2eme package

---
+++ p_vue_text.adb
@@ -0,0 +1,7 @@
+with text_io; use text_io;
+with p_combinaisons; use p_combinaisons;
+
+package body p_vue_text is
+
+
+end p_vue_text;
---
+++ p_vue_text.ads
@@ -0,0 +1,16 @@
+with text_io; use text_io;
+with p_combinaisons; use p_combinaisons;
+
+
+package p_vue_text is
+
+
+procedure
+
+
+
+
+
+
+
+end p_vue_text;
