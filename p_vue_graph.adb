10b8310e3b9a7444071df7e4fb2a48e4ab1fa9d1
ZaxisFr <nilskrattinger@yahoo.fr>
2019-01-22 11:51:02 +0100

    Ajout txt acceuil

--- p_vue_graph.adb
+++ p_vue_graph.adb
@@ -10,10 +10,15 @@ package body p_vue_graph is
     fenetre:= DebutFenetre("Acceuil",500,500);
     for i in 3..7 loop
       ajouterBouton(fenetre, integer'image(i)(2..2),integer'image(i)(2..2), 75+(75*(i-3)) , 300, 50, 50);
+      changerTailleTexte(fenetre, integer'image(i)(2..2),FL_MEDIUM_SIZE);
+      changerStyleTexte(fenetre, integer'image(i)(2..2), FL_BOLD_STYLE);
     end loop;
     ajouterBouton(fenetre, "Contigue", "Solutions contigue", 35 , 375 , 200 , 50);
     ajouterBouton(fenetre, "Normal", "Toutes les solutions", 265 , 375 , 200 , 50);
     ajouterBouton(fenetre, "Fermer", "Quitter", 200 , 450 , 100 , 50);
+    ajouterTexte(fenetre, "Textintro", "Lorem ipsum dolor sit amet, tempor incididunt ut labore et dolore magna aliqua. Ut  Dui
+                                        s aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                                        pariatur.", 50, 50, 400,100);
     finFenetre(fenetre);
     montrerFenetre(fenetre);
     appuiBoutonAccueil(attendreBouton(fenetre),fenetre);
